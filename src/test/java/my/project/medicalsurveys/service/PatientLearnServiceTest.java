02d4bed86aeb34260c3045fb918a9db6388712ff
vadevid <kislitsun-van@mail.ru>
2024-04-11 06:57:41 +0300

    update project

--- src/test/java/my/project/medicalsurveys/service/PatientLearnServiceTest.java
+++ src/test/java/my/project/medicalsurveys/service/PatientLearnServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class PatientLearnServiceTest {
 
     @Autowired
