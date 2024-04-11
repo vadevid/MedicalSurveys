30d21921faa112151ef6a5447683c5949a9684c2
vadevid <kislitsun-van@mail.ru>
2024-04-11 06:41:52 +0300

    update test

--- src/main/java/my/project/medicalsurveys/MedicalSurveysApplication.java
+++ src/main/java/my/project/medicalsurveys/MedicalSurveysApplication.java
@@ -2,7 +2,9 @@ package my.project.medicalsurveys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @SpringBootApplication
 public class MedicalSurveysApplication {
 
